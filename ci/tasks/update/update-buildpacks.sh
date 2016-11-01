fc7e6a872ec51179482b216ec725e6ac29bc5a3a
virtmerlin <merlin@sense.net>
2016-11-01 12:45:10 -0500

    test if cf cli is in image

--- ci/tasks/update/update-buildpacks.sh
+++ ci/tasks/update/update-buildpacks.sh
@@ -2,11 +2,17 @@
 
 set -e
 
-# Log Into CF
+# install cf cli
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+sudo apt-get update
+sudo apt-get install cf-cli
 
+# log into CF
 cf api https://${cf_api} --skip-ssl-validation
 cf login -u ${cf_user} -p ${cf_password} -o system -s system
 
+# list buildpacks
 cf buildpacks
 
 exit 1
