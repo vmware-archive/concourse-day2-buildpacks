bfd0f96b79e7949bfb4a2cff119037178c70f806
virtmerlin <merlin@sense.net>
2016-11-18 09:18:32 -0600

    removed https prefix for testing

--- ci/tasks/update/update-buildpacks.sh
+++ ci/tasks/update/update-buildpacks.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-# install cf cli
+# install latest cf cli
 sudo wget -O /tmp/cfcli.deb "https://cli.run.pivotal.io/stable?release=debian64&version=6.22.2&source=github-rel"
 sudo dpkg -i /tmp/cfcli.deb && apt-get install -f
 
 # log into CF
-cf api https://${cf_api} --skip-ssl-validation
+cf api ${cf_api} --skip-ssl-validation
 cf login -u ${cf_user} -p ${cf_password} -o system -s system
 
 # list buildpacks
