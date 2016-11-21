bba2da824739cfc68b474a97613f35dd89c9635e
virtmerlin <merlin@sense.net>
2016-11-21 11:25:28 -0600

    refactor update & test into seperate jobs

--- ci/tasks/update/update-buildpacks.sh
+++ ci/tasks/update/update-buildpacks.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
 set -e
 
-# install latest cf cli
+# install cf cli
 sudo wget -O /tmp/cfcli.deb "https://cli.run.pivotal.io/stable?release=debian64&version=6.22.2&source=github-rel"  > /dev/null 2>&1
 sudo dpkg -i /tmp/cfcli.deb && apt-get install -f  > /dev/null 2>&1
 
@@ -11,59 +9,24 @@ sudo dpkg -i /tmp/cfcli.deb && apt-get install -f  > /dev/null 2>&1
 ###############################
 function fn_auth_cli {
 
-  cf api ${cf_api} --skip-ssl-validation > /dev/null 2>&1
-  cf login -u ${cf_user} -p ${cf_password} -o system -s system > /dev/null 2>&1
+  cf api ${sandbox_cf_api} --skip-ssl-validation > /dev/null 2>&1
+  cf login -u ${sandbox_cf_user} -p ${sandbox_cf_password} -o system -s system > /dev/null 2>&1
 
 }
 
-function fn_get_buildpack_id {
+function fn_update_buildpack {
 
-   local buildpack=${1}
+  bp_version=$(cat $(ls -d *buildpack-release*)/version)
+  bp_id=$(fn_get_buildpack_id ${buildpack})
+  bp_position=$(cf curl /v2/buildpacks/${bp_id} | jq .entity.position | tr -d '\"')
+  bp_fname=$(cf curl /v2/buildpacks/${bp_id} | jq .entity.filename | tr -d '\"')
 
-   my_cmd="cf curl /v2/buildpacks | jq '.resources[] | select(.entity.name==\"${buildpack}\") | .' | jq .metadata.guid | tr -d '\"'"
-   eval $my_cmd
+  cp_cmd="cp $(ls -d *buildpack-release*)/release.tgz ./${buildpack}-${bp_version}.zip"
+  (eval $cp_cmd)
 
-}
-
-function fn_restage_apps_with_buildpack {
-
-  echo "Starting Healthcheck Jobs ..."
-
-  local buildpack_id=${1}
-  local pids=""
-  failcount=0
-  declare -a apps
-  my_cmd="cf curl /v2/apps | jq '.resources[] | select(.entity.detected_buildpack_guid==\"${buildpack_id}\") | .metadata.guid' | tr -d '\"'"
-  apps=$(eval $my_cmd)
-  for x in ${apps[@]}; do
-      echo "Restaging ${x}"
-      cf curl -X POST /v2/apps/$x/restage > /dev/null 2>&1
-      $PWD/concourse-day2-buildpacks/ci/tasks/update/fn_healthcheck.sh $x &
-      pids+="$!:$x"
-  done
-
-  echo "Wating for Healthcheck Jobs to finish ..."
-
-  for my_job in ${pids}; do
-    my_pid=$(echo $my_job | awk -F ":" '{print$1}')
-    my_app=$(echo $my_job | awk -F ":" '{print$2}')
-    wait $my_pid
-    if [ $? -eq 0 ]; then
-        echo "SUCCESS - Healthcheck for apps $my_app exited with a status of $?"
-    else
-        echo "FAILED - Healthcheck for apps $my_app exited with a status of $?"
-        (( failcount++ ))
-    fi
-  done
-
-  #if [ $FAIL -gt 0 ]; then
-  #  exit 1
-  #fi
+  exit 1
 
 }
-
-
-###### Main Logic
 
 function fn_trigger {
 
@@ -71,9 +34,9 @@ function fn_trigger {
   echo "Will work on ... ${buildpack}"
   fn_auth_cli
   buildpack_id=$(fn_get_buildpack_id "${buildpack}")
-  fn_restage_apps_with_buildpack "${buildpack_id}"
 
 }
+
 
 case ${buildpack} in
     java_buildpack_offline)
