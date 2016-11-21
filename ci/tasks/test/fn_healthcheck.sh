2e273e843e141d374b412e6f90ce2e4e0e3af433
virtmerlin <merlin@sense.net>
2016-11-21 11:25:38 -0600

    refactor update & test into seperate jobs

---
+++ ci/tasks/test/fn_healthcheck.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Checks for restaged apps to enter a running state
+
+function fn_check_app_health {
+
+  local app_id=${1}
+  timeout=600
+  sleep 3
+
+  for (( x=0; x < $timeout; x++ )); do
+
+        app_instance_state_cmd="cf curl /v2/apps/${app_id}/stats | jq -r 'keys[] as \$k | [\$k,(.[\$k].state)] | @csv' | tr -d '\"'"
+        ai_count=0
+        for y in $(eval ${app_instance_state_cmd}); do
+                (( ai_count++ ))
+        done
+        healthy_count=0
+
+        for x in $(eval ${app_instance_state_cmd}); do
+            app_instance_id=$(echo $x | awk -F "," '{print$1}')
+            app_instance_state=$(echo $x | awk -F "," '{print$2}')
+            if [[ ${app_instance_state} == "RUNNING" ]]; then
+              echo ${app_id}":instance[${app_instance_id}]-state:"${app_instance_state}
+              (( healthy_count++ ))
+            else
+              echo "${app_id}:instance[${app_instance_id}]-state:${app_instance_state}"
+              echo "${app_id} :instance[${app_instance_id}] Not Healthy Yet...."
+            fi
+        done
+
+        if [[ ${healthy_count} -eq ${ai_count} ]]; then
+          return 0
+        fi
+        sleep 1
+  done
+
+  if [[ ! ${healthy_count} -eq ${ai_count} ]]; then
+    echo "AppID:${1} Not Running" 1>&2
+    exit 1
+  else
+    echo "AppID:${1} Running :) "
+    exit 0
+  fi
+}
+
+fn_check_app_health ${1}
---
+++ ci/tasks/test/test-buildpacks.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+set -e
+
+# install cf cli
+sudo wget -O /tmp/cfcli.deb "https://cli.run.pivotal.io/stable?release=debian64&version=6.22.2&source=github-rel"  > /dev/null 2>&1
+sudo dpkg -i /tmp/cfcli.deb && apt-get install -f  > /dev/null 2>&1
+
+###############################
+# Functions   #################
+###############################
+function fn_auth_cli {
+
+  cf api ${cf_api} --skip-ssl-validation > /dev/null 2>&1
+  cf login -u ${cf_user} -p ${cf_password} -o system -s system > /dev/null 2>&1
+
+}
+
+function fn_get_buildpack_id {
+
+   local buildpack=${1}
+
+   my_cmd="cf curl /v2/buildpacks | jq '.resources[] | select(.entity.name==\"${buildpack}\") | .' | jq .metadata.guid | tr -d '\"'"
+   eval $my_cmd
+
+}
+
+function fn_restage_apps_with_buildpack {
+
+  echo "Starting Healthcheck Jobs ..."
+
+  local buildpack_id=${1}
+  local pids=""
+  failcount=0
+  declare -a apps
+  my_cmd="cf curl /v2/apps | jq '.resources[] | select(.entity.detected_buildpack_guid==\"${buildpack_id}\") | .metadata.guid' | tr -d '\"'"
+  apps=$(eval $my_cmd)
+  for x in ${apps[@]}; do
+      echo "Restaging ${x}"
+      cf curl -X POST /v2/apps/$x/restage > /dev/null 2>&1
+      $PWD/concourse-day2-buildpacks/ci/tasks/test/fn_healthcheck.sh $x &
+      pids+="$!:$x"
+  done
+
+  echo "Wating for Healthcheck Jobs to finish ..."
+
+  for my_job in ${pids}; do
+    my_pid=$(echo $my_job | awk -F ":" '{print$1}')
+    my_app=$(echo $my_job | awk -F ":" '{print$2}')
+    wait $my_pid
+    if [ $? -eq 0 ]; then
+        echo "SUCCESS - Healthcheck for apps $my_app exited with a status of $?"
+    else
+        echo "FAILED - Healthcheck for apps $my_app exited with a status of $?"
+        (( failcount++ ))
+    fi
+  done
+
+}
+
+
+###### Main Logic
+
+function fn_trigger {
+
+  declare -a apps
+  echo "Will work on ... ${buildpack}"
+  fn_auth_cli
+  buildpack_id=$(fn_get_buildpack_id "${buildpack}")
+  fn_restage_apps_with_buildpack "${buildpack_id}"
+
+}
+
+case ${buildpack} in
+    java_buildpack_offline)
+      fn_trigger
+      ;;
+    go_buildpack)
+      fn_trigger
+      ;;
+    nodejs_buildpack)
+      fn_trigger
+      ;;
+    *)
+      echo "BuildPack ${buildpack} Not Yet Instrumented!!!"
+      exit 1
+      ;;
+esac
